bc234bacf2e6da1f209579264e7e0c39f7a56ad0
TorroSoft <torrozapata@gmail.com>
2021-12-30 18:01:09 -0700

    The validation when a vacante is created is already finished

--- controller/VacantesController.js
+++ controller/VacantesController.js
@@ -15,17 +15,45 @@ exports.formularioNuevaVacante = async (req,res,next)=>{
 }
 exports.agregarVacante =  async(req,res,next)=>{
     try {
-        const autor = res.locals.usuario._id;
-        let data = req.body;
-        data.skills = req.body.skills.split(",");
-        const vacante = new Vacante(data);
-        vacante.autor = autor;
-        
-        const result = await vacante.save();
-        if(result){
-            res.redirect(`/vacantes/${result.url}`);
+
+        req.sanitizeBody("titulo").escape();
+        req.sanitizeBody("empresa").escape();
+        req.sanitizeBody("ubicacion").escape();
+        req.sanitizeBody("salario").escape();
+        req.sanitizeBody("contrato").escape();
+        req.sanitizeBody("skills").escape();
+
+        req.checkBody("titulo", "Agregar titulo a la vacante").notEmpty();
+        req.checkBody("empresa", "Agregar empresa a la vacante").notEmpty();
+        req.checkBody("ubicacion", "Agregar ubicación a la vacante").notEmpty();
+        req.checkBody("salario", "Agregar salario a la vacante").notEmpty();
+        req.checkBody("contrato", "Agregar contrato a la vacante").notEmpty();
+        req.checkBody("skills", "Agregar habilidades requeridas a la vacante").notEmpty();
+
+        const errores = req.validationErrors();
+
+        if(!errores){
+            const autor = res.locals.usuario._id;
+            let data = req.body;
+            data.skills = req.body.skills.split(",");
+            const vacante = new Vacante(data);
+            vacante.autor = autor;
+            
+            const result = await vacante.save();
+            if(result){
+                res.redirect(`/vacantes/${result.url}`);
+            }else{
+                res.redirect("/");
+            }
         }else{
-            res.redirect("/");
+            req.flash("error", errores.map(error=>error.msg));
+            res.render("nuevaVacante",{
+                namaPage: "Nueva Vacante",
+                tagLine: "Llena el formulario y publica tu vacante",
+                cerrarSesion: true,
+                nombre: res.locals.usuario.nombre,
+                mensajes: req.flash()
+            })
         }
         
     } catch (error) {
@@ -68,10 +96,43 @@ exports.formEditarVacante = async(req,res,next)=>{
 }
 exports.editarVacante = async(req,res,next)=>{
     try {
-        const vacanteActualizada = req.body;
-        vacanteActualizada.skills = vacanteActualizada.skills.split(",");
-        const vacante = await Vacante.findOneAndUpdate({url: req.params.url},vacanteActualizada,{new:true});
-        res.redirect(`/vacantes/${vacante.url}`);
+
+        req.sanitizeBody("titulo").escape();
+        req.sanitizeBody("empresa").escape();
+        req.sanitizeBody("ubicacion").escape();
+        req.sanitizeBody("salario").escape();
+        req.sanitizeBody("contrato").escape();
+        req.sanitizeBody("skills").escape();
+
+        req.checkBody("titulo", "Agregar titulo a la vacante").notEmpty();
+        req.checkBody("empresa", "Agregar empresa a la vacante").notEmpty();
+        req.checkBody("ubicacion", "Agregar ubicación a la vacante").notEmpty();
+        req.checkBody("salario", "Agregar salario a la vacante").notEmpty();
+        req.checkBody("contrato", "Agregar contrato a la vacante").notEmpty();
+        req.checkBody("skills", "Agregar habilidades requeridas a la vacante").notEmpty();
+
+        const errores = req.validationErrors();
+
+        if(!errores){
+            const vacanteActualizada = req.body;
+            vacanteActualizada.skills = vacanteActualizada.skills.split(",");
+            const vacante = await Vacante.findOneAndUpdate({url: req.params.url},vacanteActualizada,{new:true});
+            res.redirect(`/vacantes/${vacante.url}`);
+        }else{
+            req.flash("error", errores.map(error=>error.msg));
+            const {url} = req.params;
+            const vacante = await Vacante.findOne({url}).lean();
+
+            res.render("editarVacante",{
+                namePage: "Editando Vacante",
+                vacante,
+                cerrarSesion: true,
+                nombre: res.locals.usuario.nombre,
+                mensajes: req.flash()
+            });
+        }
+
+
     } catch (error) {
         next();
     }
